1a4d4a9bfc5fd749543986f45a233452b19389b8
nayal <nayal@etic.yt>
2025-02-17 12:36:05 +0300

    Set up authentication middleware

--- controllers/stuff.js
+++ controllers/stuff.js
@@ -64,19 +64,33 @@ exports.modifyThing = (req, res, next) => {
 };
 
 exports.deleteThing = (req, res, next) => {
-    Thing.deleteOne({_id: req.params.id}).then(
+  Thing.findOne({ _id: req.params.id }).then(
+    (thing) => {
+      if (!thing) {
+        return res.status(404).json({
+          error: new Error('Objet non trouvé !')
+        });
+      }
+      if (thing.userId !== req.auth.userId) {
+        return res.status(401).json({
+          error: new Error('Requête non autorisée !')
+        });
+      }
+      Thing.deleteOne({_id: req.params.id}).then(
         () => {
-            res.status(200).json({
-                message: 'Deleted!'
-            });
+          res.status(200).json({
+            message: 'Deleted!'
+          });
         }
-    ).catch(
+      ).catch(
         (error) => {
-            res.status(400).json({
-                error: error
-            });
+          res.status(400).json({
+            error: error
+          });
         }
-    );
+      );
+    }
+  );
 };
 
 exports.getAllStuff = (req, res, next) => {
--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
 exports.signup = (req, res, next) => {
@@ -26,38 +27,42 @@ exports.signup = (req, res, next) => {
 };
 
 exports.login = (req, res, next) => {
-    User.findOne({ email: req.body.email }).then(
-        (user) => {
-            if (!user) {
-                return res.status(401).json({
-                    error: new Error('User not found!')
-                });
-            }
-            bcrypt.compare(req.body.password, user.password).then(
-                (valid) => {
-                    if (!valid) {
-                        return res.status(401).json({
-                            error: new Error('Incorrect password!')
-                        });
-                    }
-                    res.status(200).json({
-                        userId: user._id,
-                        token: 'token'
-                    });
-                }
-            ).catch(
-                (error) => {
-                    res.status(500).json({
-                        error: error
-                    });
-                }
-            );
-        }
-    ).catch(
-        (error) => {
-            res.status(500).json({
-                error: error
+  User.findOne({ email: req.body.email }).then(
+    (user) => {
+      if (!user) {
+        return res.status(401).json({
+          error: new Error('User not found!')
+        });
+      }
+      bcrypt.compare(req.body.password, user.password).then(
+        (valid) => {
+          if (!valid) {
+            return res.status(401).json({
+              error: new Error('Incorrect password!')
             });
+          }
+          const token = jwt.sign(
+            { userId: user._id },
+            'RANDOM_TOKEN_SECRET',
+            { expiresIn: '24h' });
+          res.status(200).json({
+            userId: user._id,
+            token: token
+          });
         }
-    );
-};
+      ).catch(
+        (error) => {
+          res.status(500).json({
+            error: error
+          });
+        }
+      );
+    }
+  ).catch(
+    (error) => {
+      res.status(500).json({
+        error: error
+      });
+    }
+  );
+}
