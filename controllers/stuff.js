65bdf21fd5756d09520a13e020b41668c0f3a77a
nayal <nayal@etic.yt>
2025-02-17 16:00:06 +0300

    End of course

--- controllers/stuff.js
+++ controllers/stuff.js
@@ -1,4 +1,5 @@
 const Thing = require('../models/thing');
+const fs = require('fs');
 
 exports.createThing = (req, res, next) => {
   req.body.thing = JSON.parse(req.body.thing);
@@ -80,31 +81,24 @@ exports.modifyThing = (req, res, next) => {
 };
 
 exports.deleteThing = (req, res, next) => {
-  Thing.findOne({ _id: req.params.id }).then(
+  Thing.findOne({_id: req.params.id}).then(
     (thing) => {
-      if (!thing) {
-        return res.status(404).json({
-          error: new Error('Objet non trouvé !')
-        });
-      }
-      if (thing.userId !== req.auth.userId) {
-        return res.status(401).json({
-          error: new Error('Requête non autorisée !')
-        });
-      }
-      Thing.deleteOne({_id: req.params.id}).then(
-        () => {
-          res.status(200).json({
-            message: 'Deleted!'
-          });
-        }
-      ).catch(
-        (error) => {
-          res.status(400).json({
-            error: error
-          });
-        }
-      );
+      const filename = thing.imageUrl.split('/images/')[1];
+      fs.unlink('images/' + filename, () => {
+        Thing.deleteOne({_id: req.params.id}).then(
+          () => {
+            res.status(200).json({
+              message: 'Deleted!'
+            });
+          }
+        ).catch(
+          (error) => {
+            res.status(400).json({
+              error: error
+            });
+          }
+        );
+      });
     }
   );
 };
